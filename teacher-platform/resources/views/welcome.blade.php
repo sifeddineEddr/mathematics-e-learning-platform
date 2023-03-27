a72c279f231bbe1775eae11dbccc0fdfdda554d4
sifeddineEddr <sifeddine.edr@gmail.com>
2023-03-27 11:05:17 +0000

    tailwindcss set up

--- teacher-platform/resources/views/welcome.blade.php
+++ teacher-platform/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +129,17 @@
             </div>
         </div>
     </body>
+</html> --}}
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
 </html>
