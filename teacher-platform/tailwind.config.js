d1e781a0250ea466d209d7efe17f9418d0048fbf
sifeddineEddr <sifeddine.edr@gmail.com>
2023-03-30 11:19:59 +0000

    login page layout

--- teacher-platform/tailwind.config.js
+++ teacher-platform/tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'brand-red': '#C00005',
+                'brand-light-ish-brown': '#FAF2EF',
+              },
         },
     },
 
