b74c1896d5dbdc955237b6227cd485bf83263f15
sifeddineEddr <sifeddine.edr@gmail.com>
2023-04-03 09:50:32 +0000

    flowbite plugin set up

--- teacher-platform/resources/views/teacher/students.blade.php
+++ teacher-platform/resources/views/teacher/students.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="/images/logo-icon.svg" type="image/x-icon">
     <title>رياضياتي</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-brand-light-ish-brown w-full h-screen">
     <x-side-bar />
     <div dir="rtl" class="h-screen abolute left-0 sm:w-[75%] lg:w-[80%] flex items-center justify-around">
-        
+
     </div>
 </body>
 
