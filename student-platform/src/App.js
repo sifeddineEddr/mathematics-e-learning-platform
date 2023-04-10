aa81803074993ddc74fcb121778fb62c7ff9d4f0
sifeddineEddr <sifeddine.edr@gmail.com>
2023-04-10 12:54:57 +0000

    folder structure refactor

--- student-platform/src/App.js
+++ student-platform/src/App.js
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div>
+      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <img src="/images/landing-animation.gif" alt="r" />
+    
+    </div>
   );
 }
 
