22b721c7467d903bd61f293286c87a42776dbb40
sifeddineEddr <sifeddine.edr@gmail.com>
2023-04-14 19:00:02 +0000

    register

--- teacher-platform/database/factories/UserFactory.php
+++ teacher-platform/database/factories/UserFactory.php
@@ -25,8 +25,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'first_name' => fake()->name(1),
+            'last_name' => fake()->name(1),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'two_factor_secret' => null,
